acf728ada11840c3d26c2fcebdf4ee96b1e03feb
peter7891 <127941206+peter7891@users.noreply.github.com>
2023-09-24 13:15:21 +0200

    Update build.rs

--- build.rs
+++ build.rs
@@ -23,7 +23,7 @@ fn main() {
 
     let dst = autotools::Config::new(LIB_GC_DIR)
         .cflag(format!(
-            "-I{} -L/lib/x86_64-linux-gnu -lpthread -fPIC -D POINTER_MASK=0x5",
+            "-I{} -L/lib/x86_64-linux-gnu -lpthread -fPIC -D POINTER_MASK=0x7ffffffffffffff",
             dst.join("include").display()
         ))
         .build();
@@ -60,7 +60,7 @@ fn main() {
         .profile("Release")
         .define("BUILD_SHARED_LIBS", "FALSE")
         .cflag(format!(
-            "-I{} -DPOINTER_MASK=0x5",
+            "-I{} -DPOINTER_MASK=0x7ffffffffffffff",
             libatomic_include_path
         ))
         .build();
